edf26ac6145bd077a94284fe1b9967c309fdda5e
its_shyam640 <shyamvashishtha640@gmail.com>
2021-08-10 13:43:58 +0530

    Did minor changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
+const chalk = require('chalk');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URL,{
    useNewUrlParser : true,
    useCreateIndex : true,
    useFindAndModify : false
-});
+}).then(() => console.log(chalk.bold.bgRed('Success : Connected to Database!')))
+   .catch((e) => console.log('Error : ',e));
